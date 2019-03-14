a2b6a99bc367e15436ca1922b1678c28efb9f805
Jordy Nelson Samuel <jordysamuel@gmail.com>
2019-03-14 14:44:57 +0800

    added some logic to postmicro

--- PySerial/postmicro.py
+++ PySerial/postmicro.py
@@ -1,13 +1,50 @@
 import requests, time
 import paho.mqtt.client as mqtt
 
+class Status:
+    pir = False
+    pirVal = 0
+
+    ultra = False
+    ultraVal = 1.0
+
+    temp = False
+    tempVal = 0
+
+    light = False
+    lightVal = 0
+
 url = "iot.jordysamuel.com"
 client = mqtt.Client("server")
 def on_message(client, userdata, message):
-    print("message received", str(message.payload.decode('utf-8')))
-    print("message topic=",message.topic)
-    print("message qos=",message.qos)
-    print("message retain flag=",message.retain)
+    if message.topic == "pi1/temp":
+        Status.temp = True
+        tempInt = int(message.payload.decode('utf-8'))
+        Status.tempVal = tempInt
+        print("tempInt: ", tempInt)
+    elif message.topic == "pi1/ultra":
+        Status.ultra = True
+        ultraFloat = float(message.payload.decode('utf-8'))
+        Status.ultraVal = ultraFloat
+        print("ultraFloat: ", ultraFloat)
+    elif message.topic == "pi1/pir":
+        Status.pir = True
+        pirInt = int(message.payload.decode('utf-8'))
+        Status.pirVal = pirInt
+        print("pirInt: ", pirInt)
+    elif message.topic == "pi1/light":
+        Status.light = True
+        lightInt = int(message.payload.decode('utf-8'))
+        Status.lightVal = lightInt
+        print('lightInt: ', lightInt)
+    
+    #print("message received", str(message.payload.decode('utf-8')))
+    #print("message topic=",message.topic)
+    #print("message qos=",message.qos)
+    #print("message retain flag=",message.retain)
+
+    if Status.light and Status.pir and Status.temp and Status.ultra:
+        i = 1
 client.on_message = on_message
 client.connect(url)
 client.subscribe("pi1/temp")
