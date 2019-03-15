90afdcc2b10e3b523e9301e08204070ebb875f7f
Jordy Nelson Samuel <jordysamuel@gmail.com>
2019-03-15 14:19:32 +0800

    added try except

--- PySerial/readmicro.py
+++ PySerial/readmicro.py
@@ -13,25 +13,28 @@ url = "iot.jordysamuel.com"
 client = mqtt.Client("pi1")
 
 while True:
-    data = s.readline()
-    converted = data.decode('utf-8')
-    if converted:
-        client.connect(url)
-        print(converted)
-        if 'light' in converted:
-            lightVal = converted[6:]
-            lightInt = int(lightVal)
-            client.publish("pi1/light", lightInt)
-        elif 'temp' in converted:
-            tempVal = converted[5:]
-            tempInt = int(tempVal)
-            client.publish("pi1/temp", tempInt)
-        elif 'ultra' in converted:
-            ultraVal = converted[6:]
-            ultraFloat = float(ultraVal)
-            client.publish("pi1/ultra", ultraFloat)
-        elif 'pir' in converted:
-            pirVal = converted[4:]
-            pirInt = int(pirVal)
-            client.publish("pi1/pir", pirInt)
-        client.disconnect()
+    try:
+        data = s.readline()
+        converted = data.decode('utf-8')
+        if converted:
+            client.connect(url)
+            print(converted)
+            if 'light' in converted:
+                lightVal = converted[6:]
+                lightInt = int(lightVal)
+                client.publish("pi1/light", lightInt)
+            elif 'temp' in converted:
+                tempVal = converted[5:]
+                tempInt = int(tempVal)
+                client.publish("pi1/temp", tempInt)
+            elif 'ultra' in converted:
+                ultraVal = converted[6:]
+                ultraFloat = float(ultraVal)
+                client.publish("pi1/ultra", ultraFloat)
+            elif 'pir' in converted:
+                pirVal = converted[4:]
+                pirInt = int(pirVal)
+                client.publish("pi1/pir", pirInt)
+            client.disconnect()
+    except: 
+        print("Error, reconnecting")
