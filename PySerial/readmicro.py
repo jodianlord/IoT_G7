23f282bb836db67121ef21d532d567acb306bf8e
Jordy Nelson Samuel <jordysamuel@gmail.com>
2019-03-15 10:50:45 +0800

    reconnect every time

--- PySerial/readmicro.py
+++ PySerial/readmicro.py
@@ -1,7 +1,7 @@
 import serial
 import paho.mqtt.client as mqtt
 
-port = "COM6"
+port = "/dev/ttyACM0"
 baud = 115200
 
 s = serial.Serial(port)
@@ -11,25 +11,28 @@ s.baudrate = baud
 url = "iot.jordysamuel.com"
 
 client = mqtt.Client("pi1")
-client.connect(url)
 
 while True:
     data = s.readline()
     converted = data.decode('utf-8')
-    print(converted)
-    if 'light' in converted:
-        lightVal = converted[6:]
-        lightInt = int(lightVal)
-        client.publish("pi1/light", lightInt)
-    elif 'temp' in converted:
-        tempVal = converted[5:]
-        tempInt = int(tempVal)
-        client.publish("pi1/temp", tempInt)
-    elif 'ultra' in converted:
-        ultraVal = converted[6:]
-        ultraFloat = float(ultraVal)
-        client.publish("pi1/ultra", ultraFloat)
-    elif 'pir' in converted:
-        pirVal = converted[4:]
-        pirInt = int(pirVal)
-        client.publish("pi1/pir", pirInt)
+    if converted:
+        client = mqtt.Client("pi1")
+        client.connect(url)
+        print(converted)
+        if 'light' in converted:
+            lightVal = converted[6:]
+            lightInt = int(lightVal)
+            client.publish("pi1/light", lightInt)
+        elif 'temp' in converted:
+            tempVal = converted[5:]
+            tempInt = int(tempVal)
+            client.publish("pi1/temp", tempInt)
+        elif 'ultra' in converted:
+            ultraVal = converted[6:]
+            ultraFloat = float(ultraVal)
+            client.publish("pi1/ultra", ultraFloat)
+        elif 'pir' in converted:
+            pirVal = converted[4:]
+            pirInt = int(pirVal)
+            client.publish("pi1/pir", pirInt)
+        client.disconnect()
