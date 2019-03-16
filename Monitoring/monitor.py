b4b26329ad9615e34fe9ede3f8f4d8551e6df53b
Tan Kok Heng <kokheng.tan.2016@sis.smu.edu.sg>
2019-03-16 14:13:36 +0800

    added try catch for request exception
    catch request exception

--- Monitoring/monitor.py
+++ Monitoring/monitor.py
@@ -10,9 +10,14 @@ chat_id = -1001155518486
 
 while True:
     now = dt.datetime.now()
-    content = requests.get("http://iot.jordysamuel.com:8000/api/facility/reading/").json()
-    created_at = content['data']['items'][0]['created_at'][0:-6]
-    created = datetime.strptime(created_at, '%Y-%m-%dT%H:%M:%S.%f')
-    if now - created > dt.timedelta(minutes=10):
-        bot.send_message(chat_id=chat_id, text="<b>Data Not collected</b>" + " at " + created_at, parse_mode=telegram.ParseMode.HTML)
-    time.sleep(600)
+    try:
+        content = requests.get("http://iot.jordysamuel.com:8000/api/facility/reading/").json()
+        created_at = content['data']['items'][0]['created_at'][0:-6]
+        created = datetime.strptime(created_at, '%Y-%m-%dT%H:%M:%S.%f')
+        if now - created > dt.timedelta(minutes=10):
+            bot.send_message(chat_id=chat_id, text="<b>Data Not collected</b>" + " at " + created_at, parse_mode=telegram.ParseMode.HTML)
+    except requests.exceptions.RequestException as e:
+        bot.send_message(chat_id=chat_id, text="Server is down", parse_mode=telegram.ParseMode.HTML)
+    finally:
+        time.sleep(600)
+
