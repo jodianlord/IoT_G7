ca58fdf8bedd8c5e5bdcacf6d23babb36b5ad21c
CLARENCE NEO _ <clarenceneo.2016@smu.edu.sg>
2019-03-28 22:21:13 +0800

    Update settings.py

--- IoT_Occupancy/IoT_Occupancy/settings.py
+++ IoT_Occupancy/IoT_Occupancy/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'NAME': 'sensors',
         'USER': 'django',
         'PASSWORD': 'password',
-        'HOST': 'localhost',
+        'HOST': 'iot.jordysamuel.com',
         'PORT': '5432'
     }
 }
